a16c48fb416a7ff1bec0d4a18b3a638c2b942a2b
Kenny W. Lino <kennywlino@gmail.com>
2022-10-17 12:20:38 -0700

    First day demo.

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Person from './Person.js';
+
+class Main extends React.Component {
+   render() {
+    return (
+        <>
+        <main className='main-component'>
+            <Person 
+                name="Zoe" 
+                course="301d90" 
+            />
+            <Person name="Marc" />
+            <Person name="Kenny" />
+            <Person name="Hunter" />
+            <Person name="Dennis" />
+        </main>
+        </>
+    )
+   } 
+}
+
+export default Main;
